a86fff962b3920a9d473d5d2aba5554f9dd5cd11
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-09 16:07:54 +0100

    Ajoute une route pour obtenir le profil
    ...avec des fausses valeurs pour le moment

--- src/serveur.ts
+++ src/serveur.ts
@@ -1,1 +1,1 @@
-console.log("Démarré")
+console.log("Démarré");
--- tests/serveur.spec.ts
+++
@@ -1,5 +0,0 @@
-import {test} from "node:test"
-import * as assert from "assert";
-test("fonctionne", () => {
-    assert.equal(2, 2)
-})
