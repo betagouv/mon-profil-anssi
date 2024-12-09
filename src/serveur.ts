27993f90ebc983e90a9d65a213a24e2a737dee14
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-09 16:48:21 +0100

    Injecte l'entrepôt de profils

--- src/serveur.ts
+++ src/serveur.ts
@@ -1,7 +1,13 @@
 import { creeServeur } from "./mpa.js";
+import { Profil } from "./profil.js";
 
 const port = process.env.PORT || 8080;
 
-creeServeur().listen(port, () => {
+const entrepotProfil = {
+  parEmail(email: string): Profil | undefined {
+    return undefined;
+  },
+};
+creeServeur({ entrepotProfil }).listen(port, () => {
   console.log("L'API MonProfilANSSI est démarrée");
 });
--- tests/mpa.spec.ts
+++ tests/mpa.spec.ts
@@ -1,23 +1,45 @@
-import { describe, it, test } from "node:test";
+import { beforeEach, describe, it } from "node:test";
 import * as assert from "assert";
 import request from "supertest";
 import { creeServeur } from "../src/mpa.js";
+import { Express } from "express";
+import { EntrepotProfilMemoire } from "./entrepotProfilMemoire.js";
 
 describe("Sur demande du profil", function () {
+  let serveur: Express;
+
+  beforeEach(() => {
+    const entrepotProfil = new EntrepotProfilMemoire();
+    const jeanDujardin = {
+      email: "jean@beta.fr",
+      nom: "Dujardin",
+      prenom: "Jean",
+      entite: {
+        siret: "DINUM",
+        departement: "33",
+      },
+      domainesSpecialite: ["RSSI", "JURI"],
+      telephone: "0607080910",
+    };
+    entrepotProfil.ajoute(jeanDujardin);
+    serveur = creeServeur({ entrepotProfil });
+  });
+
   it("répond 200", async function () {
-    const reponse = await request(creeServeur())
-      .get("/profil?email=connu@beta.fr")
+    const reponse = await request(serveur)
+      .get("/profil?email=jean@beta.fr")
       .set("Accept", "application/json");
 
     assert.equal(reponse.status, 200);
   });
 
   it("renvoie les données du profil", async () => {
-    const reponse = await request(creeServeur())
-      .get("/profil?email=connu@beta.fr")
+    const reponse = await request(serveur)
+      .get("/profil?email=jean@beta.fr")
       .set("Accept", "application/json");
 
     assert.deepEqual(reponse.body, {
+      email: "jean@beta.fr",
       nom: "Dujardin",
       prenom: "Jean",
       entite: {
@@ -30,7 +52,7 @@ describe("Sur demande du profil", function () {
   });
 
   it("répond 404 lorsque le profil est inconnu", async () => {
-    const reponse = await request(creeServeur())
+    const reponse = await request(serveur)
       .get("/profil?email=inconnu@beta.fr")
       .set("Accept", "application/json");
 
