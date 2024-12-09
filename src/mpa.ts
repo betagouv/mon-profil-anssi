aa35c03eff15b1b95794819eb96371a62c2bfaa5
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-09 16:56:15 +0100

    Renvoie une erreur lorsque l’email n’est pas fournie

--- src/mpa.ts
+++ src/mpa.ts
@@ -9,11 +9,16 @@ const creeServeur = ({ entrepotProfil }: ConfigurationServeur) => {
   const app = express();
 
   app.get("/profil", (requete, reponse) => {
-    if (requete.query.email === "inconnu@beta.fr") {
+    if (!requete.query.email) {
+      reponse.sendStatus(400);
+      return;
+    }
+    const profil = entrepotProfil.parEmail(requete.query.email as string);
+    if (!profil) {
       reponse.sendStatus(404);
       return;
     }
-    const profil = entrepotProfil.parEmail("jean@beta.fr");
+
     reponse.send(profil);
   });
 
