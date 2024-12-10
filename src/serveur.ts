8fb340c834fc5f3c0dd98d8753295269c5e73dc8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-10 14:21:18 +0100

    Extrais l'entrepôt de profils postgres

--- src/serveur.ts
+++ src/serveur.ts
@@ -1,17 +1,12 @@
 import { creeServeur } from "./mpa";
-import { Profil } from "./profil";
+import { entrepotProfilPostgres } from "./entrepotProfilPostgres";
 import { fabriqueMiddleware } from "./middleware";
 
 const port = process.env.PORT || 8080;
 
-const entrepotProfil = {
-  parEmail(email: string): Profil | undefined {
-    return undefined;
-  },
-};
-creeServeur({ entrepotProfil, middleware: fabriqueMiddleware() }).listen(
-  port,
-  () => {
-    console.log("L'API MonProfilANSSI est démarrée");
-  },
-);
+creeServeur({
+  entrepotProfil: entrepotProfilPostgres,
+  middleware: fabriqueMiddleware(),
+}).listen(port, () => {
+  console.log("L'API MonProfilANSSI est démarrée");
+});
