9b8051402250b045bbabd15e704faa8f22109490
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-10 16:14:47 +0100

    Ajuste le port de l’API

--- src/serveur.ts
+++ src/serveur.ts
@@ -2,11 +2,11 @@ import { creeServeur } from "./mpa";
 import { entrepotProfilPostgres } from "./entrepotProfilPostgres";
 import { fabriqueMiddleware } from "./middleware";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3001;
 
 creeServeur({
   entrepotProfil: entrepotProfilPostgres,
   middleware: fabriqueMiddleware(),
 }).listen(port, () => {
-  console.log("L'API MonProfilANSSI est démarrée");
+  console.log(`L'API MonProfilANSSI est démarrée sur le port ${port}`);
 });
