3b3f405f79d1ba50891bd066f6507d7a10e2948f
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-10 16:15:05 +0100

    Lis en base de données le profil

--- src/ressourceProfil.ts
+++ src/ressourceProfil.ts
@@ -1,15 +1,18 @@
 import express from "express";
 import { ConfigurationServeur } from "./configurationServeur";
 
-const ressourceProfil = ({ entrepotProfil, middleware }: ConfigurationServeur) => {
+const ressourceProfil = ({
+  entrepotProfil,
+  middleware,
+}: ConfigurationServeur) => {
   const routeur = express.Router();
 
-  routeur.get("/", middleware.aseptise("email"), (requete, reponse) => {
+  routeur.get("/", middleware.aseptise("email"), async (requete, reponse) => {
     if (!requete.query.email) {
       reponse.sendStatus(400);
       return;
     }
-    const profil = entrepotProfil.parEmail(requete.query.email as string);
+    const profil = await entrepotProfil.parEmail(requete.query.email as string);
     if (!profil) {
       reponse.sendStatus(404);
       return;
