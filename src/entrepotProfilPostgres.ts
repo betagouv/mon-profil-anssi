9a435202acfc6434c2bf429d8e71c403397b56b1
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-11 11:01:52 +0100

    Mutualise la configuration Knex

--- src/entrepotProfilPostgres.ts
+++ src/entrepotProfilPostgres.ts
@@ -1,15 +1,15 @@
 import { Profil } from "./profil";
 import knex from "knex";
 import { EntrepotProfil } from "./entrepotProfil";
+import { knexConfig } from "./knexfile";
+
+type NodeEnv = "development" | "production";
 
 export const entrepotProfilPostgres: EntrepotProfil = {
   async parEmail(email: string): Promise<Profil | undefined> {
-    const db = knex({
-      client: "pg",
-      connection: "postgres://postgres@mpa-db/mpa",
-      pool: { min: 0, max: 10 },
-      migrations: { tableName: "knex_migrations" },
-    });
+    const nodeEnv = process.env.NODE_ENV;
+    if (!nodeEnv) return;
+    const db = knex(knexConfig[nodeEnv as NodeEnv]);
     return db("profils").where({ email }).first().select();
   },
 };
