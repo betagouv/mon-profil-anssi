54451c9f53f6e3961f30673476f48251c76952c8
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-12 17:33:00 +0100

    Mets à jour le profil dans l'entrepôt
    ... Nous choisissons de « tordre » l’entrepôt mémoire pour mimer l’entrepôt postgres au mieux. Dans un monde idéal DDD avec un ORM, nous n’aurions pas besoin de mettre à jour car l’entrepôt postgres se comporterait comme une liste en mémoire.

--- src/metier/entrepotProfil.ts
+++ src/metier/entrepotProfil.ts
@@ -2,5 +2,8 @@ import { Profil } from "./profil";
 
 export interface EntrepotProfil {
   parEmail(email: string): Promise<Profil | undefined>;
+
   ajoute(profil: Profil): Promise<void>;
+
+  metsAJour(profil: Profil): Promise<void>;
 }
