b0e25f15cc8a00e881fb49c692aa6fb0f01baef0
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-12 10:53:54 +0100

    Inscris le profil au service

--- src/metier/profil.ts
+++ src/metier/profil.ts
@@ -1,5 +1,23 @@
-export interface Profil {
+export class Profil {
+  email: string;
+  nom: string;
+  prenom: string;
+
+  constructor({
+    email,
+    nom,
+    prenom,
+  }: {
     email: string;
     nom: string;
     prenom: string;
+  }) {
+    this.email = email;
+    this.nom = nom;
+    this.prenom = prenom;
+  }
+
+  estInscritA(service: string): boolean{
+    return true;
+  }
 }
--- tests/api/ressourceInscription.spec.ts
+++ tests/api/ressourceInscription.spec.ts
@@ -5,6 +5,7 @@ import { creeServeur } from "../../src/api/mpa";
 import { fabriqueMiddleware } from "../../src/api/middleware";
 import request from "supertest";
 import assert from "assert";
+import { Profil } from "../../src/metier/profil";
 
 describe("Sur demande d'inscription", () => {
   let serveur: Express;
@@ -21,11 +22,14 @@ describe("Sur demande d'inscription", () => {
     });
 
     assert.equal(reponse.status, 201);
-    assert.deepEqual(await entrepotProfil.parEmail("jean@beta.fr"), {
-      email: "jean@beta.fr",
-      prenom: "",
-      nom: "",
-    });
+    assert.deepEqual(
+      await entrepotProfil.parEmail("jean@beta.fr"),
+      new Profil({
+        email: "jean@beta.fr",
+        prenom: "",
+        nom: "",
+      }),
+    );
   });
 
   it("aseptise les paramètres", async () => {
@@ -40,5 +44,19 @@ describe("Sur demande d'inscription", () => {
       prenom: "&gt;Jean",
       nom: "&lt;Dujardin",
     });
+  });
+
+  it("inscrit l'utilisateur au service", async () => {
+    await request(serveur)
+      .post("/inscription")
+      .send({
+        email: "jean@beta.fr",
+        prenom: "Jean",
+        nom: "Dujardin",
+      })
+      .set("x-id-client", "mss");
+
+    const profil = await entrepotProfil.parEmail("jean@beta.fr");
+    assert.equal(profil!.estInscritA("mss"), true);
   });
 });
