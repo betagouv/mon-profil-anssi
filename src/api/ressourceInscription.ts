68f73184d7c78c352c209dab565e2e9d517a17e3
Fabien Bézagu <fabien.bezagu@beta.gouv.fr>
2024-12-12 10:03:03 +0100

    Aseptise le profil inscrit

--- src/api/ressourceInscription.ts
+++ src/api/ressourceInscription.ts
@@ -1,15 +1,23 @@
-import {Router, Request, Response} from "express";
-import {ConfigurationServeur} from "./configurationServeur";
+import { Router, Request, Response } from "express";
+import { ConfigurationServeur } from "./configurationServeur";
 
-const ressourceInscription = ({entrepotProfil, middleware,}: ConfigurationServeur) => {
-    const routeur = Router();
+const ressourceInscription = ({
+  entrepotProfil,
+  middleware,
+}: ConfigurationServeur) => {
+  const routeur = Router();
 
-    routeur.post("/", async (requete: Request, reponse: Response) => {
-        await entrepotProfil.ajoute(requete.body);
-        reponse.sendStatus(201);
-    });
+  routeur.post(
+    "/",
+    middleware.aseptise("email", "nom", "prenom"),
+    async (requete: Request, reponse: Response) => {
+      const { email, nom, prenom } = requete.body;
+      await entrepotProfil.ajoute({ email, nom, prenom });
+      reponse.sendStatus(201);
+    },
+  );
 
-    return routeur;
+  return routeur;
 };
 
-export {ressourceInscription};
+export { ressourceInscription };
--- tests/api/ressourceInscription.spec.ts
+++ tests/api/ressourceInscription.spec.ts
@@ -1,30 +1,44 @@
-import {beforeEach, describe, it} from "node:test";
-import {Express} from "express";
-import {EntrepotProfilMemoire} from "../persistance/entrepotProfil.memoire";
-import {creeServeur} from "../../src/api/mpa";
-import {fabriqueMiddleware} from "../../src/api/middleware";
+import { beforeEach, describe, it } from "node:test";
+import { Express } from "express";
+import { EntrepotProfilMemoire } from "../persistance/entrepotProfil.memoire";
+import { creeServeur } from "../../src/api/mpa";
+import { fabriqueMiddleware } from "../../src/api/middleware";
 import request from "supertest";
 import assert from "assert";
 
 describe("Sur demande d'inscription", () => {
-    let serveur: Express;
-    let entrepotProfil: EntrepotProfilMemoire;
+  let serveur: Express;
+  let entrepotProfil: EntrepotProfilMemoire;
 
-    beforeEach(() => {
-        entrepotProfil = new EntrepotProfilMemoire();
-        serveur = creeServeur({entrepotProfil, middleware: fabriqueMiddleware()});
+  beforeEach(() => {
+    entrepotProfil = new EntrepotProfilMemoire();
+    serveur = creeServeur({ entrepotProfil, middleware: fabriqueMiddleware() });
+  });
+
+  it("ajoute le profil", async () => {
+    const reponse = await request(serveur).post("/inscription").send({
+      email: "jean@beta.fr",
     });
 
-    it("ajoute le profil", async () => {
-        const reponse = await request(serveur)
-            .post("/inscription")
-            .send({
-                email: 'jean@beta.fr'
-            });
-
-        assert.equal(reponse.status, 201);
-        assert.deepEqual(await entrepotProfil.parEmail('jean@beta.fr'), {
-            email: 'jean@beta.fr'
-        });
+    assert.equal(reponse.status, 201);
+    assert.deepEqual(await entrepotProfil.parEmail("jean@beta.fr"), {
+      email: "jean@beta.fr",
+      prenom: "",
+      nom: "",
     });
+  });
+
+  it("aseptise les paramètres", async () => {
+    await request(serveur).post("/inscription").send({
+      email: "  jean@beta.fr",
+      prenom: ">Jean",
+      nom: "<Dujardin",
+    });
+
+    assert.deepEqual(await entrepotProfil.parEmail("jean@beta.fr"), {
+      email: "jean@beta.fr",
+      prenom: "&gt;Jean",
+      nom: "&lt;Dujardin",
+    });
+  });
 });
