de82a5eb5a239fc78f35f8de74582fada1a02190
Stéfanie Loiseleur <stefanie.loiseleur@beta.gouv.fr>
2024-12-13 10:13:16 +0100

    Implémente la sauvegarde de profil
    ... dans l'adaptateur postgres

--- src/persistance/entrepotProfil.postgres.ts
+++ src/persistance/entrepotProfil.postgres.ts
@@ -5,11 +5,20 @@ import { knexConfig } from "./knexfile";
 
 type NodeEnv = "development" | "production";
 
+const connexion = () => {
+  const nodeEnv = process.env.NODE_ENV;
+  if (!nodeEnv)
+    throw new Error("Configuration invalide : NODE_ENV non renseigné.");
+  return knex(knexConfig[nodeEnv as NodeEnv]);
+};
 export const entrepotProfilPostgres: EntrepotProfil = {
+  async ajoute(profil: Profil): Promise<void> {
+    const db = connexion();
+    const { email, prenom, nom } = profil;
+    await db("profils").insert({ email, prenom, nom });
+  },
   async parEmail(email: string): Promise<Profil | undefined> {
-    const nodeEnv = process.env.NODE_ENV;
-    if (!nodeEnv) return;
-    const db = knex(knexConfig[nodeEnv as NodeEnv]);
+    const db = connexion();
     return db("profils").where({ email }).first().select();
   },
 };
