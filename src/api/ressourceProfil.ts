a369c22beb93337e1686c775e343e53810238ce1
Xavier Maso <xavier.maso@beta.gouv.fr>
2026-05-05 11:30:21 +0200

    [SOIN] Extrait les schémas dans un fichier dédié
    ...pour en faciliter le partage entre les différentes ressources.

--- src/api/ressourceProfil.ts
+++ src/api/ressourceProfil.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
-import * as z from "zod";
 import { ConfigurationServeur } from "./configurationServeur";
 import { versProfilAPI } from "./profilAPI";
 import { ErreurDonneesObligatoiresManquantes } from "../metier/erreurDonneesObligatoiresManquantes";
 import { fabriqueServiceInscription } from "../metier/serviceInscription";
+import {
+  schemaLectureProfil,
+  schemaCorpsPourMiseAJourProfil,
+  schemaParametresPourMiseAJourProfil,
+} from "./schemas";
 
 const ressourceProfil = ({
   entrepotProfil,
@@ -15,8 +19,6 @@ const ressourceProfil = ({
     adaptateurHorloge,
     entrepotProfil,
   });
-
-  const schemaLectureProfil = z.object({params: z.strictObject({email: z.email()})});
 
   routeur.get(
     "/:email",
@@ -53,45 +55,11 @@ const ressourceProfil = ({
     },
   );
 
-  const schemaCorpsPourMiseAJour = z.object({
-    body: z.strictObject({
-      email: z.email("L'adresse email à mettre à jour est invalide").optional(),
-      nom: z.string('Le nom est invalide').max(1024).optional(),
-      prenom: z.string('Le prénom est invalide').max(1024).optional(),
-      organisation: z.any().superRefine((valeur, contexte) => {
-        if (typeof valeur === 'object' && valeur !== null) {
-          if (Object.keys(valeur).length === 0) return;
-
-          if (valeur.nom === undefined || typeof valeur.nom !== 'string' || valeur.nom.length > 1024) {
-            contexte.addIssue({ code: "custom", message: "Le nom de l'organisation est invalide" })
-          }
-
-          if (valeur.siret === undefined || typeof valeur.siret !== 'string' || !/^\d{14}/.test(valeur.siret)) {
-            contexte.addIssue({ code: "custom", message: "Le siret de l'organisation est invalide" })
-          }
-
-          if (valeur.departement === undefined || typeof valeur.departement !== 'string' || valeur.departement.length in [2, 3]) {
-            contexte.addIssue({ code: "custom", message: "Le département de l'organisation est invalide" })
-          }
-
-          return z.NEVER;
-        }
-
-        contexte.addIssue({ code: "custom", message: "L'organisation est invalide" })
-        return z.NEVER;
-      }).optional(),
-      domainesSpecialite: z.array(z.string('Les domaines de spécialité sont invalides').max(42), 'Les domaines de spécialité sont invalides').max(100, 'Les domaines de spécialité sont invalides').optional(),
-      telephone: z.string('Le numéro de téléphone est invalide').max(20, 'Le numéro de téléphone est invalide').optional(),
-    }),
-  });
-
   routeur.put(
     "/:email",
     middleware.decodeJeton(),
-    middleware.valideRequete(z.object({params: z.strictObject({
-      email: z.email("L'adresse email existante est invalide"),
-    })})),
-    middleware.valideRequete(schemaCorpsPourMiseAJour),
+    middleware.valideRequete(schemaParametresPourMiseAJourProfil),
+    middleware.valideRequete(schemaCorpsPourMiseAJourProfil),
     async (requete: Request, reponse) => {
       // #swagger.tags = ['Profil']
       // #swagger.summary = "Crée ou mets à jour le profil avec les informations"
